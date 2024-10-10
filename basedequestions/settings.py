446447b73026dcd51a2bc39ed882b1f6f4ea1e98
malaury lemaitre salmon <malaury.lemaitresalmon@sciencespo.fr>
2024-10-10 17:32:10 +0200

    update static root

--- basedequestions/settings.py
+++ basedequestions/settings.py
@@ -109,7 +109,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'app/static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static'),  # Chemin vers le répertoire des fichiers statiques
+]
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
