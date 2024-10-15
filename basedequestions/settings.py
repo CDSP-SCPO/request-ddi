4c2bc9b416c18eede91995983d8d3c8d2967ddde
malaury lemaitre salmon <malaury.lemaitresalmon@sciencespo.fr>
2024-10-15 14:34:26 +0200

    add static

--- basedequestions/settings.py
+++ basedequestions/settings.py
@@ -113,7 +113,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
+    BASE_DIR / 'app/static',
+    BASE_DIR / 'static',
 ]
 
 
