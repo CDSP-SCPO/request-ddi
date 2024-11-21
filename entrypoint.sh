a5b5e55d3830a9d19a235aed2696ce8a46db3eb7
malaury lemaitre salmon <malaury.lemaitresalmon@sciencespo.fr>
2024-11-21 14:40:13 +0100

    migration flush

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 python manage.py showmigrations
-python manage.py flush --noinput*
+python manage.py flush --noinput
 
 # Lancer les migrations Django
 echo "Lancement des migrations Django..."
