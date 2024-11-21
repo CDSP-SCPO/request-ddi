eac54805d47a66d306a6d31f257c4ee4178897f5
Willyf17 <william.feraux@gmail.com>
2024-11-21 15:25:09 +0100

    remove delete table from entrypoint

--- delete_table.py
+++
@@ -1,9 +0,0 @@
-from django.db import connection
-
-def drop_all_tables():
-    with connection.cursor() as cursor:
-        cursor.execute("DROP SCHEMA public CASCADE;")
-        cursor.execute("CREATE SCHEMA public;")
-        print("Toutes les tables ont été supprimées.")
-
-drop_all_tables()
