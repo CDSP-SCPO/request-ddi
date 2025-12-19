497d2555ab3a33a00957680a3734791379f0b7c6
william feraux <william.feraux@sciencespo.fr>
2025-12-19 10:47:17 +0100

    feat: :sparkles: Links to guide and github repo in popover homepage

--- request_ddi/static/js/base.js
+++ request_ddi/static/js/base.js
@@ -1,36 +1,43 @@
 $(document).ready(function () {
     let $currentPopover = null;
-
     $('[data-toggle="custom-popover"]').on('click', function (e) {
         e.stopPropagation();
 
-        // Fermer le popover actuel s'il existe
         if ($currentPopover) {
             $currentPopover.remove();
             $currentPopover = null;
             return;
         }
 
-        // Créer le popover
         const content = $(this).next('.custom-popover-content').html();
         const $popover = $('<div class="custom-popover-box ui-tooltip"></div>').html(content);
 
         $('body').append($popover);
 
-        // Positionner à gauche de l'élément cliqué
         const offset = $(this).offset();
         const elementHeight = $(this).outerHeight();
         const popoverWidth = $popover.outerWidth();
+        const popoverHeight = $popover.outerHeight();
+
+        let topPosition = offset.top + elementHeight / 2 - popoverHeight / 2 - 50;
+
+        const windowHeight = $(window).height();
+        const maxTopPosition = windowHeight - popoverHeight - 10;
+
+        if (topPosition + popoverHeight > windowHeight) {
+            topPosition = maxTopPosition;
+        }
 
         $popover.css({
-            top: offset.top + elementHeight / 2 - $popover.outerHeight() / 2 - 50,
-            left: offset.left - popoverWidth - 20
+            position: 'absolute',
+            top: topPosition,
+            left: offset.left - popoverWidth - 20,
+            zIndex: 9999
         });
 
         $currentPopover = $popover;
     });
 
-    // Fermer si on clique ailleurs
     $(document).on('click', function () {
         if ($currentPopover) {
             $currentPopover.remove();
